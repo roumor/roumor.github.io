65651a866f7ead8aca894883d0ddecd26db741d6
Movsar Makhmutov <m.movsar@gmail.com>
2022-10-21 15:25:54 +0500

    added form

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# super-form
+# Форма+
+
+## Теория
+- Изучите [DOM MutationObserver](https://habr.com/ru/post/202366/). 
+- Изучите [LocalStorage](https://learn.javascript.ru/localstorage).
+
+## Практическая работа
--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // script
+
+const observer = new MutationObserver(mutations => {
+    mutations.forEach(mutation => {
+        if (mutation.addedNodes.length || mutation.removedNodes.length) {
+            // ваш код
+        }
+    })
+})
+
+// Поменяйте название переменной elem
+const elem = document.querySelector('ЗДЕСЬ_КЛАСС_КОТОРЫЙ_ХОТИТЕ_ОТСЛЕДИТЬ')
+observer.observe(elem, {
+    childList: true
+})
