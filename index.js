458905e693929e80557859e1fb1209c8a4effb16
Александр Фетисов <roumor@gmail.com>
2022-12-13 23:05:39 +0700

    release 1

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ let name = document.querySelector("#name"),
   email = document.querySelector("#email"),
   btn = document.querySelector(".btn"),
   users = document.querySelector(".users"),
-  clear = document.querySelector(".clear");
+  clear = document.querySelector(".clear"),
+  tochange = "";
 
 // Объект для localStorage
 let storage = JSON.parse(localStorage.getItem("users")) || {};
@@ -37,9 +38,14 @@ function getData(e) {
 
   localStorage.setItem("users", JSON.stringify(storage));
 
+  if (tochange && tochange !== key) {
+    deleteCard(tochange);
+    tochange = "";
+  }
+
   rerenderCard(JSON.parse(localStorage.getItem("users")));
 
-  return data;
+  //return data;
 }
 
 function createCard({ name, secondName, email }) {
@@ -99,6 +105,13 @@ function rerenderCard(storage) {
   });
 }
 
+function deleteCard(clicked) {
+  let ls = JSON.parse(localStorage.getItem("users"));
+  delete ls[clicked];
+  localStorage.setItem("users", JSON.stringify(ls));
+  window.location.reload();
+}
+
 function setListeners() {
   const del = document.querySelectorAll(".delete");
   const change = document.querySelectorAll(".change");
@@ -111,20 +124,22 @@ function setListeners() {
       clicked = n.getAttribute("data-delete");
 
       const outer = document.querySelector(`[data-out="${clicked}"]`);
-
       console.log("=== outer", outer);
-
+      deleteCard(clicked);
       let ls = JSON.parse(localStorage.getItem("users"));
-      delete ls[clicked];
-      localStorage.setItem("users", JSON.stringify(ls));
       rerenderCard(ls);
-      window.location.reload();
     });
   });
 
   change.forEach((n) => {
     n.addEventListener("click", () => {
       console.log("=== ПРИМЕНИТЬ кнопка");
+      const clicked = n.getAttribute("data-change");
+      let ls = JSON.parse(localStorage.getItem("users"));
+      name.value = ls[clicked].name;
+      secondName.value = ls[clicked].secondName;
+      email.value = ls[clicked].email;
+      tochange = clicked;
     });
   });
 }
